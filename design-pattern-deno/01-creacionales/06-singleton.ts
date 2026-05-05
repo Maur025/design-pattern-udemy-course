073b5ad271261237939162401ad297a9fca5c068
maur025 <mmoyapalli@gmail.com>
2026-05-05 01:48:40 -0400

    fear:deno add pattern prototype, copy inmutability and singleton

--- design-pattern-deno/01-creacionales/06-singleton.ts
+++ design-pattern-deno/01-creacionales/06-singleton.ts
@@ -1,3 +1,4 @@
+import { COLORS } from "../helpers/colors.ts";
 /**
  * ! Singleton:
  * Es un patrón de diseño creacional que garantiza que una clase
@@ -9,3 +10,64 @@
  *
  * https://refactoring.guru/es/design-patterns/singleton
  */
+
+class DragonBalls {
+	private static instance: DragonBalls;
+	private ballsCollected: number;
+
+	private constructor() {
+		this.ballsCollected = 0;
+	}
+
+	public static getInstance(): DragonBalls {
+		if (!DragonBalls.instance) {
+			DragonBalls.instance = new DragonBalls();
+			console.log("%cLas pelotas del Dragón han sido creadas!", COLORS.green);
+		}
+
+		return DragonBalls.instance;
+	}
+
+	collectBall(): void {
+		if (this.ballsCollected < 7) {
+			this.ballsCollected++;
+			console.log(`Pelota recolectada. Total de esferas: ${this.ballsCollected}`);
+			return;
+		}
+
+		console.log("Ya se han recolectado las 7 esferas del Dragón! Invoca a Shenlong");
+	}
+
+	summonShenlong() {
+		if (this.ballsCollected === 7) {
+			console.log("Shenlong ha sido invocado, pide tu deseo!");
+			this.ballsCollected = 0;
+			return;
+		}
+
+		console.log(`Aún faltan ${7 - this.ballsCollected} pelotas para invocar a Shenlong`);
+	}
+}
+
+function main() {
+	const gokuDragonBalls = DragonBalls.getInstance();
+
+	gokuDragonBalls.collectBall();
+	gokuDragonBalls.collectBall();
+	gokuDragonBalls.collectBall();
+
+	gokuDragonBalls.summonShenlong();
+
+	const vegetaDragonBalls = DragonBalls.getInstance();
+
+	vegetaDragonBalls.collectBall();
+	vegetaDragonBalls.collectBall();
+	vegetaDragonBalls.collectBall();
+	vegetaDragonBalls.collectBall();
+
+	gokuDragonBalls.summonShenlong();
+
+	vegetaDragonBalls.summonShenlong();
+}
+
+main();
